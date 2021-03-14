460bcf652fdfd4bae6c8a30c46e87a0d82824599
NicolasMonteroM <nicolasmontero42@gmail.com>
2021-03-13 23:08:40 -0500

    first commit

--- app/src/main/java/com/example/quiz1_eco/Nexo.java
+++ app/src/main/java/com/example/quiz1_eco/Nexo.java
@@ -2,13 +2,27 @@ package com.example.quiz1_eco;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.widget.Button;
 
 public class Nexo extends AppCompatActivity {
+
+    private Button nextBtn;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_nexo);
+
+        nextBtn = findViewById(R.id.nextBtn);
+
+        nextBtn.setOnClickListener(
+                (v) -> {
+                    Intent i = new Intent(this, Symptoms.class);
+                    startActivity(i);
+
+                }
+        );
     }
 }
